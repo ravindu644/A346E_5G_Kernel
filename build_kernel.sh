45356cb51d27b64ee51421bc0dfda9b3b21b4da2
Ravindu Deshan <Droidcasts@protonmail.com>
2025-01-10 14:50:04 +0000

    build script: re-design

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,18 +1,47 @@
 #!/bin/bash
+export ARCH=arm64
+export RDIR="$(pwd)"
+export KBUILD_BUILD_USER="@ravindu644"
 
 #init ksu next
 git submodule init && git submodule update
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export ARCH=arm64
+#export toolchain paths
+export BUILD_CROSS_COMPILE="${RDIR}/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-"
+export BUILD_CC="${RDIR}/toolchain/clang/host/linux-x86/clang-r383902/bin/clang"
 
+#output dir
+if [ ! -d "${RDIR}/out" ]; then
+    mkdir -p "${RDIR}/out"
+fi
 
-export KCFLAGS=-w
-export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+#build dir
+if [ ! -d "${RDIR}/build" ]; then
+    mkdir -p "${RDIR}/build"
+else
+    rm -rf "${RDIR}/build" && mkdir -p "${RDIR}/build"
+fi
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a34x_defconfig a34.config
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+#build options
+export ARGS="
+-C $(pwd) \
+O=$(pwd)/out \
+-j$(nproc) \
+ARCH=arm64 \
+CROSS_COMPILE=${BUILD_CROSS_COMPILE} \
+CC=${BUILD_CC} \
+CLANG_TRIPLE=aarch64-linux-gnu- \
+KCFLAGS=-w \
+CONFIG_SECTION_MISMATCH_WARN_ONLY=y \
+"
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+#build kernel image
+build_kernel(){
+    make ${ARGS} clean && make ${ARGS} mrproper
+    make ${ARGS} a34x_defconfig a34.config
+    make ${ARGS} menuconfig
+    make ${ARGS} || exit 1
+    cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+}
+
+build_kernel
